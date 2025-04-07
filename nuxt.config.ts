a9415fecf2b345098fd50b152b330dc5877b2b8b
yaonkey <thesuperuserstyle@gmail.com>
2025-04-08 01:45:29 +0300

    updating

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/yaonkey.github.io/',
+    buildAssetsDir: 'assets'
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['~/assets/css/style.css'],
